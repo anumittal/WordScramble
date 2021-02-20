cd07f055712244c2b81e17b3764cfab308e9ba36
Anu Mittal <anu22mittal@gmail.com>
2021-02-20 17:07:16 +0800

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anu Mittal on 20/02/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
